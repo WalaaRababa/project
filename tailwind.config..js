e03ec13fd3273c12a5855cddfe723e182ae48015
WalaaRababa <walaarababa@gmail.com>
2023-04-29 14:56:31 +0300

    add tailwind

--- tailwind.config..js
+++ tailwind.config..js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
+    darkMode: 'class',
     theme: {
-      extend: {},
+        fontFamily: {
+          burtons: "burtons",
+          poppins: ["Poppins", "sans-serif"],
+      
+        
+      },
     },
     plugins: [],
   }
